5242f286e5cb00bedd61244c6604e743212d4c93
Mingcan Li <limingcanjim@gmail.com>
2023-04-11 13:53:08 -0500

    Create test.cpp

---
+++ DataProcessing/test.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Mingcan Li on 4/7/23.
+//
+#include "TiingoApi.cpp"
+#include "QCoreApplication"
+#include "time.h"
+
+int main(int argc, char *argv[]){
+    QCoreApplication a(argc, argv);
+    DataSource* ds = new TiingoApi("your-api-key-here");
+    ds->setTickerSymbol("btcusd",{"tiingo/crypto/"});
+    ds->setHistoryLen(300);
+    ds->setSampleFreq(5);
+    ds->startLive();
+    return a.exec();
+}
